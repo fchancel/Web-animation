1d8949e1d686af7c42b5162a467be36719ffb848
CHANCELADE Florian <florian.chancelade@gmail.com>
2021-09-05 10:24:32 +0200

    Refactore project

--- js/bubble-swing.js
+++ js/bubble-swing.js
@@ -1,33 +1,31 @@
-
 const randomInt = (min, max) =>
-Math.floor(Math.random() * (max - min + 1)) + min
+  Math.floor(Math.random() * (max - min + 1)) + min;
 
 const lstColor = [
-    "#f1c40f",
-    "#3498db",
-    "#27ae60",
-    "#9b59b6",
-    "#ecf0f1",
-    "#16a085",
-    "#e74c3c",
+  "#f1c40f",
+  "#3498db",
+  "#27ae60",
+  "#9b59b6",
+  "#ecf0f1",
+  "#16a085",
+  "#e74c3c",
 ];
-
 
 const body = document.querySelector("body");
 
 const btns = document.querySelectorAll(".btn");
-btns.forEach(elt => {
-    elt.addEventListener("click", (e)=>{
-        e.stopPropagation();
-        const activeElt = document.querySelector('.active');
-        activeElt.className = "btn";
-        elt.className = "btn active"
-    })
-})
+btns.forEach((elt) => {
+  elt.addEventListener("click", (e) => {
+    e.stopPropagation();
+    const activeElt = document.querySelector(".active");
+    activeElt.className = "btn";
+    elt.className = "btn active";
+  });
+});
 
-const littleCircle = (posX, posY, color) => {
+const bubble = (posX, posY, color) => {
   const div = document.createElement("div");
-  div.className = "little-circle";
+  div.className = "bubble";
   div.style.cssText = `
 
         top: ${posY - 30}px;
@@ -55,11 +53,9 @@ const littleCircle = (posX, posY, color) => {
 body.addEventListener("click", (e) => {
   const limit = document.querySelector(".active");
   for (let i = 0; i < limit.dataset.value; i++) {
-    const div = littleCircle(e.clientX, e.clientY, lstColor[randomInt(0, 6)]);
+    const div = bubble(e.clientX, e.clientY, lstColor[randomInt(0, 6)]);
     setTimeout(() => {
-        div.remove();
-      }, 500);
+      div.remove();
+    }, 500);
   }
-
 });
-
